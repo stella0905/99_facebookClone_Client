a0b6203077a1bdfbb1d3a33ba8fc08ffa91150f6
Leezero3 <fuddl4525@naver.com>
2023-05-12 23:07:25 +0900

    first commit

---
+++ src/redux/modules/example.js
@@ -0,0 +1,13 @@
+const initialState = {
+    number: 0,
+};
+
+const Slice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {},
+});
+
+export const {} = Slice.actions;
+
+export default Slice.reducer;
