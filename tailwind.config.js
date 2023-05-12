66f96cc8bea962416e875022bcfc1b5a61b29306
Leezero3 <fuddl4525@naver.com>
2023-05-12 23:20:44 +0900

    tailwind 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,js}"],
+    // content: [
+    //   "./src/**/*.{js,jsx,ts,tsx}",
+    // ],
 
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
+// module.exports = {
+//   content: ['./src/**/*.{js,jsx}'],
+//   theme: {
+//     extend: {
+//       colors: {
+//         brand: '#E74646',
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
