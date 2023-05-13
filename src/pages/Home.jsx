61a5ee7268fdca63663e45db20e26266544fcadc
stella0905 <o122233@naver.com>
2023-05-14 06:56:09 +0900

    Design 메인 페이지 수정중 [게시물작성,전체게시물]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import MainFeed from "components/mainFeed/MainFeed";
 import React from "react";
 
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div class="bg-gray-100">
+        <MainFeed/>
+        </div>
+    );
 };
 
 export default Home;
