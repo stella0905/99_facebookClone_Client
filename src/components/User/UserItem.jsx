80c82848853e90b5607bb5a66849463b26496f4a
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-05-16 20:22:13 +0900

    [Feat] 친구추가 API구현 (#20)
    * [Remove]  EditRemove파일 삭제 및 수정 모달 ui구현
    
    * [Design] 드래그앤드롭 추가완료
    
    * [Feat] 검색기능 API연결
    
    * [Feat] 친구추가 API 구현

--- src/components/User/UserItem.jsx
+++ src/components/User/UserItem.jsx
@@ -1,11 +1,21 @@
+import { followUser } from 'components/axios/users';
 import React from 'react';
 import { FiUserPlus } from 'react-icons/fi';
+import { useMutation } from 'react-query';
 
 const UserItem = ({users}) => {
 
   const default_profile_url = '/images/default-profile-url.png';
-  const handleAddClick = () => {};
 
+  const mutation = useMutation(followUser, {
+    onSuccess: () => {
+      alert("작성 저장 성공");
+    },
+  });
+
+  const handleAddClick = (follow_id) => {
+    mutation.mutate(follow_id)
+  };
 
   const isValidUrl=(url)=>{
     const pattern = /^(https?:\/\/)?([\w.-]+)\.([a-zA-Z]{2,6})(\/[\w.-]*)*\/?$/;
@@ -28,7 +38,7 @@ const UserItem = ({users}) => {
             <p className='text-sm font-semibold leading-6 text-gray-900 mr-40'>
               {user.name}
             </p>
-            <FiUserPlus className='cursor-pointer' onClick={handleAddClick} />
+            <FiUserPlus className='cursor-pointer' onClick={()=>handleAddClick(user.user_id)} />
           </div>
         </div>
       </li>
