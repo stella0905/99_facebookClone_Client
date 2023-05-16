a9545ce1a20399b8631d6f6b81210a91e5ec3dfc
Lee YoungEun <77380372+Leezero3@users.noreply.github.com>
2023-05-16 17:26:21 +0900

    [Feat] 게시글 추가기능 구현중

--- src/components/mainFeed/MainFeed.jsx
+++ src/components/mainFeed/MainFeed.jsx
@@ -1,21 +1,18 @@
-import React, { useState } from 'react'
-import Board from './Board'
-import BoardMaker from './BoardMaker'
-
+import React from "react";
+import Board from "./Board";
+import BoardMaker from "./BoardMaker";
 
 const MainFeed = () => {
-
-
-  return (
-    <div class="text-center">
-      <div class="w-[680px] m-auto ">
-        <div class="my-5">
-        <BoardMaker/>
+    return (
+        <div class="text-center">
+            <div class="w-[680px] m-auto ">
+                <div class="my-5">
+                    <BoardMaker />
+                </div>
+                <Board />
+            </div>
         </div>
-        <Board/>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default MainFeed
+export default MainFeed;
