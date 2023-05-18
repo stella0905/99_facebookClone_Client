97a9cfb9939ce963753d4b873eeefd948354b024
stella0905 <o122233@naver.com>
2023-05-18 13:34:51 +0900

    [Feat] 프로필모달 친구추가 API구현 및 친구추가, 목록 위치 고정

--- src/components/Follow/FollowList.jsx
+++ src/components/Follow/FollowList.jsx
@@ -7,8 +7,8 @@ const FollowList = () => {
   const { data } = useQuery("followList",followList);
 
   return (
-    <div className='mr-3 my-3 w-70'>
-      <p className='text-xl font-bold leading-6 text-gray-900 my-1'>
+    <div className='mr-3 my-3 w-70 fixed right-0 '>
+      <p className='text-xl font-bold leading-6 text-gray-900 my-1 '>
         친구 목록
       </p>
       <ul>
--- src/components/mainFeed/FollowModal.jsx
+++ src/components/mainFeed/FollowModal.jsx
@@ -1,21 +1,46 @@
+import { followUser } from 'api/users';
 import React from 'react'
+import { useMutation, useQueryClient } from 'react-query';
 
-const FollowModal = (props) => {
+const FollowModal = ({data,showProfileButtons,setShowProfileButtons}) => {
+
   const default_profile_url = '/images/default-profile-url.png';
   const followIcon = '/images/followIcon.png';
+  const queryClient = useQueryClient();
+  
+  const mutation = useMutation(followUser, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("followList")
+      setShowProfileButtons(!showProfileButtons)
+    },
+    onError:(error) => {
+      alert(error.message)
+    }
+  });
 
+  const isValidUrl=(url)=>{
+    const pattern = /^(https?:\/\/)?([\w.-]+)\.([a-zA-Z]{2,6})(\/[\w.-]*)*\/?$/;
+    return pattern.test(url);
+  }
+
+  //친구추가 버튼
+  const followUserButtonHandler = (id) =>{
+    mutation.mutate(id)
+  }
   return (
     <div className="flex flex-col bg-white p-3 w-[240px] pb-4 drop-shadow">
       <div className="flex flex-row gap-2">
       <img 
         className='h-11 w-11 flex-none rounded-full self-center'
-        src={default_profile_url}
+        src={isValidUrl(data.profile_url) ? data.profile_url : default_profile_url}
         alt=''/>
-        <span>{props.userName}</span>
+        <span>{data.name}</span>
       </div>
       <div className="self-end">
         <button className="bg-[#1977F1] h-8 w-32 ">
-          <div className="flex flex-row justify-center ">
+          <div className="flex flex-row justify-center "
+          role='button'
+          onClick={()=>followUserButtonHandler(data.user_id)}>
             <img className="w-7 h-6"
             src={followIcon}/>
             <div className="self-center text-white">친구추가</div>
